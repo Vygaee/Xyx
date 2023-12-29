bb1a28d94360ee9471b9ece008fce95b77b9751c
Vooyage <148488760+Vygaee@users.noreply.github.com>
2023-12-29 08:33:44 +0700

    Update start_utils.py

--- Powers/utils/start_utils.py
+++ Powers/utils/start_utils.py
@@ -61,7 +61,7 @@ async def gen_start_kb(q: Message or CallbackQuery):
             [
                 (
                     "🗃️ Music",
-                    "",                    
+                    "^Music.",                    
                 ),
                 (
                     "Owner ❤️",
